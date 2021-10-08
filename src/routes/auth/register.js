ebe833b57745fccf17969da70aa3523535375f56
irinamerkulova <irina.merkulova@tthk.ee>
2021-10-08 11:30:09 +0300

    As a user I can register [Delivers #179780271]

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -2,7 +2,7 @@ import * as api from 'api.js';
 
 export  function post(req, res) {
     const user = req.body;
-
+//api
     api.post('users', user).then(response=>{
         if(response.user){
             req.session.user = response.user;
